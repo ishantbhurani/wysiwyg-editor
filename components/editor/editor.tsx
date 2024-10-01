0f6b1df57236b4dc5e4124524390480f7eb0d25c
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-10-02 00:50:49 +0530

    add slash command extension and menu

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -5,12 +5,25 @@ import ColorSelector from '@/components/editor/selectors/color-selector'
 import LinkSelector from '@/components/editor/selectors/link-selector'
 import NodeSelector from '@/components/editor/selectors/node-selector'
 import TextButtons from '@/components/editor/selectors/text-buttons'
+import {
+  slashCommand,
+  suggestionItems,
+} from '@/components/editor/slash-command'
 import { Separator } from '@/components/ui/separator'
-import { EditorBubble, EditorContent, EditorRoot, JSONContent } from 'novel'
+import {
+  EditorBubble,
+  EditorCommand,
+  EditorCommandEmpty,
+  EditorCommandItem,
+  EditorCommandList,
+  EditorContent,
+  EditorRoot,
+  JSONContent,
+} from 'novel'
 import { handleCommandNavigation } from 'novel/extensions'
 import { useState } from 'react'
 
-const extensions = [...defaultExtensions]
+const extensions = [...defaultExtensions, slashCommand]
 
 type EditorProps = {
   initialValue?: JSONContent
@@ -41,6 +54,32 @@ export default function Editor({ initialValue, onChange }: EditorProps) {
           onChange(editor.getJSON())
         }}
       >
+        <EditorCommand className='z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all'>
+          <EditorCommandEmpty className='px-2 text-muted-foreground'>
+            No results
+          </EditorCommandEmpty>
+          <EditorCommandList>
+            {suggestionItems.map((item) => (
+              <EditorCommandItem
+                value={item.title}
+                onCommand={(val) => item.command?.(val)}
+                className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `}
+                key={item.title}
+              >
+                <div className='flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background'>
+                  {item.icon}
+                </div>
+                <div>
+                  <p className='font-medium'>{item.title}</p>
+                  <p className='text-xs text-muted-foreground'>
+                    {item.description}
+                  </p>
+                </div>
+              </EditorCommandItem>
+            ))}
+          </EditorCommandList>
+        </EditorCommand>
+
         <EditorBubble
           tippyOptions={{ placement: 'top' }}
           className='flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl'
